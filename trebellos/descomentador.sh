f697bae7ee76e7e9fd62e6fcf7ab3fcb778ea6cf
deivis <david.cotelo.online+github@gmail.com>
2025-06-26 12:49:24 +0200

    Erros e documentaciono
    - Estaba borrando o `refsection`, o cal hai que manter
    - Agora poden descomentarse varios arquivos de golpe
    - Engadín algúns '%%%:' máis no artigo simplificado para que ao borralos
      non nos quedemos con demasiadas liñas baleiras
    - Comentei como funciona todo

--- trebellos/descomentador.sh
+++
@@ -1,8 +0,0 @@
-cat artigo_simplificado.tex | \
-    sed '/^\%\%\%\:/d'                 | \
-    sed '/^\\documentclass{revista}/d' | \
-    sed '/^\\begin{document}/d'        | \
-    sed '/^\\end{document}/d'          | \
-    sed '/^\\begin{refsection}/d'      | \
-    sed '/^\\end{refsection}/d'        > \
-    artigo_descomentado.tex
